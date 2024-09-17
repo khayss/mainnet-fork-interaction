36b4f083b04269889ab0a5ad93ed2aab0c1e591c
khayss <forendals@gmail.com>
2024-09-17 17:41:50 +0100

    update files

--- ignition/modules/ludo-game.module.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const LudoGameModule = buildModule("LudoGameModule", (m) => {
-  const ludoGame = m.contract("LudoGame");
-
-  return { ludoGame };
-});
-
-export default LudoGameModule;
